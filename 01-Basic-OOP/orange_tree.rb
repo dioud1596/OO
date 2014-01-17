317b3960a90a4a1a68a199732b482c395936fc87
Edouard <edouard.rudolf@gmail.com>
2014-01-17 12:35:35 +0100

    Exo 01

--- 01-Basic-OOP/orange_tree.rb
+++ 01-Basic-OOP/orange_tree.rb
@@ -1,23 +1,64 @@
 class OrangeTree
-  
+  #attr_accessor :height, :age, :oranges
+  # no need here => not called, we have our own fancy methods
+
   def initialize
-    # Initialize an OrangeTree object
+    @height = 0
+    @oranges = 0
+    @age = 0
+    @alive = true
   end
   
   def measure_height
-    # Gives the height of the tree if alive
+    if @alive == false
+     puts "Sorry your tree won't grow anymore, it's dead"
+    else 
+      puts "your tree is #{@height} m tall"
+    end
   end
   
   def count_the_oranges
-    # Count the oranges of the tree if alive
+    if @age < 10
+      puts "Patience, I'm only a baby tree!"
+    elsif @alive == false
+      puts "your tree is dead, there will be no more oranges"
+    else 
+      puts "The tree produced #{@oranges} oranges."
+    end
   end
   
   def pick_an_orange
-    # Pick an orange to the tree
+    if @alive  && @oranges >= 1
+       @oranges -= 1
+    puts "You now have #{@oranges}"
+    else @alive == false
+    puts "Go back to ur mom"
+    end
   end
   
   def one_year_passes
-    # Main logic goes here
+    return "i'm dead birch!" if !@alive
+
+    @age += 1
+    @height += 0.4
+    death_luck = rand(1..10)
+    
+    if @age > 30 && death_luck > 6
+      @alive = false
+      message = "i'm dead birch!"
+    elsif @age >= 10
+       @oranges += @age * 6 
+       message = "you tree is young and fruitfull.. #{@oranges} oranges this year"
+    elsif @age < 10 and @alive
+       message = "you tree is still a baby.. Wait for a while"
+    end
+   
+    return message
   end
 
 end
+
+our_tree = OrangeTree.new
+50.times() { puts our_tree.one_year_passes }
+
+
